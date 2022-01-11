08c0feaffd97d39d73156652aa399a148332ca55
Gabriel Pessoni <67373880+gpessoni@users.noreply.github.com>
2022-01-11 10:23:02 -0300

    Add files via upload

---
+++ src/components/Todo.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import { AiFillCloseCircle } from "react-icons/ai";
+import { AiFillEdit } from 'react-icons/ai';
+
+const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: ''
+  });
+  
+
+  const submitUpdate = value => {
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: ''
+    });
+  };
+
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+  }
+
+  return todos.map((todo, index) => (
+    <div
+      className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+      key={index}
+    >
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
+      <div className='icons'>
+        <AiFillCloseCircle
+          onClick={() => removeTodo(todo.id)}
+          className='delete-icon'
+        />
+        <AiFillEdit
+          onClick={() => setEdit({ id: todo.id, value: todo.text })}
+          className='edit-icon'
+        />
+      </div>
+    </div>
+  ));
+};
+
+export default Todo;
